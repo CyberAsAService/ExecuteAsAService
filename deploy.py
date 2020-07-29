f61109c936a7dfefa633b4e9dc04118b456d02af
Tal Shafir <tal.4000@gmail.com>
2020-07-29 12:57:27 +0300

    fixed deploy errors and added instuctions to make celery work on windows

--- deploy.py
+++ deploy.py
@@ -1,7 +1,16 @@
 import subprocess
-from typing import Union, List, Tuple, Any
+from typing import Any, Dict
 
-from endpoint import Endpoint
+from dataclasses import dataclass
+
+
+@dataclass
+class Endpoint:
+    """Class representing endpoint."""
+    ip_address: str
+    username: str
+    password: str
+
 
 WMI_FORMAT = 'wmic /user:{user} /password:{password} /node:{remote_host} process call create '
 PSEXEC_FORMAT = 'PsExec.exe \\\\{remote_host} -u {remote_host}\\{user} -p {password} -accepteula -h {process} \"{command}\" '
@@ -19,11 +28,10 @@ def _run_subprocess(remote_host: Endpoint, process: str, command: str) -> subpro
                                            remote_host=remote_host.ip_address,
                                            process=process,
                                            command=command)
-    print(process_command)
     return subprocess.Popen(process_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
-def run_command(remote_host: Endpoint, process: str, command: str) -> Tuple[int, Any, Any]:
+def run_command(remote_host: Endpoint, process: str, command: str) -> Dict[str, str]:
     """
     :param remote_host:
     :param process:
@@ -32,7 +40,7 @@ def run_command(remote_host: Endpoint, process: str, command: str) -> Tuple[int,
     """
 
     process = _run_subprocess(remote_host, process, command)
-    return process.wait(), (*process.communicate())
+    return dict(rc=process.wait(), out=str(process.stdout.read()), err=str(process.stderr.read()))
 
 
 if __name__ == '_main_':
--- deploy_web.py
+++ deploy_web.py
@@ -1,42 +1,37 @@
 from flask import Flask, request, jsonify
 from celery import Celery
+from celery.result import AsyncResult
 
-from MicroServices.Executor.deploy import run_command
-from endpoint import Endpoint
+from deploy import run_command, Endpoint
 
 app = Flask(__name__)
-
-
-# def make_celery(app):
-#     celery = Celery(
-#         app.import_name,
-#         backend=app.config['CELERY_RESULT_BACKEND'],
-#         broker=app.config['CELERY_BROKER_URL']
-#     )
-#     celery.conf.update(app.config)
-#
-#     class ContextTask(celery.Task):
-#         def __call__(self, *args, **kwargs):
-#             with app.app_context():
-#                 return self.run(*args, **kwargs)
-#
-#     celery.Task = ContextTask
-#     return celery
-#
-#
-# celery = make_celery(app)
+app.config.update(
+    CELERY_BROKER_URL='redis://52.167.48.81:6379/0',
+    CELERY_RESULT_BACKEND='redis://52.167.48.81:6379/0'
+)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
 
 
 @app.route('/execute', methods=['POST'])
 def execute():
     data = request.form
-    print(data)
+    task = _execute.delay(data['ip_address'], data['username'], data['password'], data['process'], data['command'])
+    return jsonify(task_id=task.id)
 
-    rc, out, err = run_command(Endpoint(data['ip_address'], data['username'], data['password']), data['process'],
-                               data['command'])
-    print(rc, out.decode(), err.decode())
-    return jsonify(rc=rc, out=out.decode(), err=err.decode())
+
+@app.route('/status/<task_id>', methods=['GET'])
+def status(task_id):
+    res = AsyncResult(task_id, app=celery)
+    if res.status in ["FAILURE", "SUCCESS"]:
+        return jsonify(**res.get())
+    else:
+        return jsonify(status=res.status)
+
+
+@celery.task()
+def _execute(ip_address, username, password, process, command):
+    return run_command(Endpoint(ip_address, username, password), process, command)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
